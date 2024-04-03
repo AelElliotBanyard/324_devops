97c201f1718f9649060aae38a2e9a8b4c48f1680
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-04-03 10:14:45 +0200

    updated code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React from 'react';
-import {Col, Container, Row} from "react-bootstrap";
+import logo from './logo.svg';
 import './App.css';
+import David from './David.png';
 
 function App() {
   return (
-    <Container>
-      <Row>
-        <Col>
-            <h1>Architecture Ref. Card 2</h1>
-            <div className="message">
-              react app ... up and running
-            </div>
-        </Col>
-        <Row>
-          <Col>
-            <div className="loop-wrapper">
-              <div className="mountain"></div>
-              <div className="hill"></div>
-              <div className="tree"></div>
-              <div className="tree"></div>
-              <div className="tree"></div>
-              <div className="rock"></div>
-              <div className="truck"></div>
-              <div className="wheels"></div>
-            </div>
-          </Col>
-        </Row>
-      </Row>
-    </Container>
+    <div className="App">
+      <header className="App-header">
+        <img src={David} className="App-logo" alt="logo" />
+        <p>
+          Welcome to React App with Docker and Github Acctions
+        </p>
+        <p>
+          This should be deployed to AWS using Github Actions
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/DaniDevOfficial"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Checkout my Github
+        </a>
+
+        <a>
+          I love la gyato
+        </a>
+      </header>
+    </div>
   );
 }
 
