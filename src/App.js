2913888c478ad31d918fd35a91d2260aceab3655
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-04-03 10:16:37 +0200

    updated code

--- src/App.js
+++ src/App.js
@@ -6,25 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={David} className="App-logo" alt="logo" />
         <p>
           Welcome to React App with Docker and Github Acctions
         </p>
         <p>
           This should be deployed to AWS using Github Actions
         </p>
-        <a
-          className="App-link"
-          href="https://github.com/DaniDevOfficial"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Checkout my Github
-        </a>
-
-        <a>
-          I love la gyato
-        </a>
       </header>
     </div>
   );
